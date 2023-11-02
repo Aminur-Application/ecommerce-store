79cf69466da10db0490a9fd2ea3237f2083f1292
Prophecyuntold <Aminurr999@gmail.com>
2023-11-02 23:03:36 +0000

    display billboards on page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+import React from "react";
 
-const HomePage = () => {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("7fa71f5c-54d5-4c39-bc08-c73340e7e642");
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;

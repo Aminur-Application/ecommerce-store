281fcf9799f755430740c695a8b6e691d922c554
Prophecyuntold <Aminurr999@gmail.com>
2023-11-04 01:21:09 +0000

    display products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,6 @@
 import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/productList";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 import React from "react";
@@ -6,11 +8,16 @@ import React from "react";
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("7fa71f5c-54d5-4c39-bc08-c73340e7e642");
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured products" items={products} />
+        </div>
       </div>
     </Container>
   );

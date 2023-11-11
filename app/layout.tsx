ab4eb82482108671a8e377c50e7746e3f914219c
Prophecyuntold <Aminurr999@gmail.com>
2023-11-11 03:10:46 +0000

    add cart and preview functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,32 @@
-import type { Metadata } from 'next'
-import { Urbanist } from 'next/font/google'
-import './globals.css'
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modalProvider";
+import ToastProvider from "@/providers/toastProvider";
 
-const inter = Urbanist({ subsets: ['latin'] })
+const inter = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Store',
-  description: 'Store',
-}
+  title: "Store",
+  description: "Store",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ToastProvider />
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- components/navbarActions.tsx
+++ components/navbarActions.tsx
@@ -3,9 +3,11 @@
 import React, { useState, useEffect } from "react";
 import Button from "./ui/button";
 import { ShoppingBag } from "lucide-react";
+import useCart from "@/hooks/useCart";
 
 const navbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
@@ -19,7 +21,9 @@ const navbarActions = () => {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
